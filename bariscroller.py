9fd24713dbb1ab77e752b0ad5601deb6cecc345a
wadda <waddagit@wadda.org>
2016-12-03 14:42:52 +1100

    Initial

--- bariscroller.py
+++ bariscroller.py
@@ -1,13 +1,4 @@
-###################################################################
-#                                                                 #
-#                     PLOTTING A LIVE GRAPH                       #
-#                  ----------------------------                   #
-#            EMBED A MATPLOTLIB ANIMATION INSIDE YOUR             #
-#            OWN GUI!                                             #
-#                                                                 #
-###################################################################
-# pinched from http://stackoverflow.com/questions/11874767/real-time-plotting-in-while-loop-with-matplotlib
-
+# coding=utf-8
 import sys
 
 import matplotlib
@@ -22,9 +13,21 @@ from matplotlib.lines import Line2D
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 import time
 import threading
+
 import bari
 
 sensor = bari.Chip()
+
+__author__ = 'Moe'
+__copyright__ = 'Copyright 2016  Moe'
+__license__ = 'MIT'
+__version__ = '0.0.2'
+# pinched and modified from http://stackoverflow.com/questions/11874767/real-time-plotting-in-while-loop-with-matplotlib
+
+XLIMIT = 3000
+YUPPER = 106000
+YLOWER = 98000
+SLEEP_FOR = .052  # Guesstimate for approx. 10 readings per second.
 
 
 def setCustomSize(x, width, height):
@@ -42,8 +45,8 @@ class CustomMainWindow(QtGui.QMainWindow):
         super(CustomMainWindow, self).__init__()
 
         # Define the geometry of the main window
-        self.setGeometry(300, 300, 800, 400)
-        self.setWindowTitle("my first window")
+        self.setGeometry(300, 300, 1000, 400)
+        self.setWindowTitle("Drummoyne Wharf")
 
         # Create FRAME_A
         self.FRAME_A = QtGui.QFrame(self)
@@ -51,12 +54,36 @@ class CustomMainWindow(QtGui.QMainWindow):
         self.LAYOUT_A = QtGui.QGridLayout()
         self.FRAME_A.setLayout(self.LAYOUT_A)
         self.setCentralWidget(self.FRAME_A)
+        #
+        # self.button_test = QtGui.QPushButton(text='Test')
+        # setCustomSize(self.button_test, 60, 30)
+        # self.button_test.clicked.connect(self.test_me())
+        self.buttonbox = QtGui.QHBoxLayout()
+        # self.FRAME_A.setLayout(self.buttonbox)
 
-        # Place the zoom button
-        self.zoomBtn = QtGui.QPushButton(text='zoom')
-        setCustomSize(self.zoomBtn, 100, 50)
-        self.zoomBtn.clicked.connect(self.zoomBtnAction)
-        self.LAYOUT_A.addWidget(self.zoomBtn, *(0, 0))
+        # IN button
+        self.zoomInBtn = QtGui.QPushButton(text='In')
+        setCustomSize(self.zoomInBtn, 60, 30)
+        self.zoomInBtn.clicked.connect(self.zoomIn)
+        self.LAYOUT_A.addWidget(self.zoomInBtn, *(1, 0))
+
+        # OUT button
+        self.zoomOutBtn = QtGui.QPushButton(text='Out')
+        setCustomSize(self.zoomOutBtn, 60, 30)
+        self.zoomOutBtn.clicked.connect(self.zoomOut)
+        self.LAYOUT_A.addWidget(self.zoomOutBtn, *(1, 1))
+
+        # x fewer button
+        self.XfewerBtn = QtGui.QPushButton(text='Less')
+        setCustomSize(self.XfewerBtn, 60, 30)
+        self.XfewerBtn.clicked.connect(self.x_fewer)
+        # self.buttonbox.addWidget(self.XfewerBtn, *(1, 0))
+
+        # x more button
+        self.XmoreBtn = QtGui.QPushButton(text='More')
+        setCustomSize(self.XmoreBtn, 60, 30)
+        self.XmoreBtn.clicked.connect(self.x_more)
+        # self.buttonbox.addWidget(self.XmoreBtn, *(1, 1))
 
         # Place the matplotlib figure
         self.myFig = CustomFigCanvas()
@@ -69,9 +96,21 @@ class CustomMainWindow(QtGui.QMainWindow):
 
         self.show()
 
-    def zoomBtnAction(self):
-        print("zoom in")
-        self.myFig.zoomIn(5)
+    def zoomIn(self):
+        print("IN zoom")
+        self.myFig.zoomIn(1000)
+
+    def zoomOut(self):
+        print("zoom OUT")
+        self.myFig.zoomOut(1000)
+
+    def x_fewer(self):
+        print("IN zoom")
+        self.myFig.lessIn(500)
+
+    def x_more(self):
+        print("zoom OUT")
+        self.myFig.moreOut(500)
 
     def addData_callbackFunc(self, value):
         # print("Add data: " + str(value))
@@ -82,36 +121,32 @@ class CustomFigCanvas(FigureCanvas, TimedAnimation):
     def __init__(self):
 
         self.addedData = []
-        print(matplotlib.__version__)
+        print('Loading...', matplotlib.__version__)
 
         # The data
-        self.xlim = 200
-        self.n = np.linspace(0, self.xlim - 1, self.xlim)
-        a = []
-        b = []
-        a.append(2.0)
-        a.append(4.0)
-        a.append(2.0)
-        b.append(4.0)
-        b.append(3.0)
-        b.append(4.0)
-        self.y = (self.n * 0.0) + 50
+        self.xlim = XLIMIT
+        self.n = np.linspace(self.xlim - 1, 0, self.xlim)
+
+        self.y = (self.n * 0.0) + YLOWER
 
         # The window
-        self.fig = Figure(figsize=(5, 5), dpi=100)
+        self.fig = Figure(figsize=(12, 2), dpi=80)
         self.ax1 = self.fig.add_subplot(111)
 
         # self.ax1 settings
-        self.ax1.set_xlabel('time')
-        self.ax1.set_ylabel('raw data')
-        self.line1 = Line2D([], [], color='blue')
+        self.ax1.set_xlabel('Readings incremented time')
+        self.ax1.set_ylabel('River Level - Raw data')
+        self.line1 = Line2D([], [], color='blue', aa=True)
         self.line1_tail = Line2D([], [], color='red', linewidth=2)
         self.line1_head = Line2D([], [], color='red', marker='o', markeredgecolor='r')
+        # self.line2 = Line2D([], [], linewidth=5, color='red', )
+
         self.ax1.add_line(self.line1)
         self.ax1.add_line(self.line1_tail)
         self.ax1.add_line(self.line1_head)
-        self.ax1.set_xlim(0, self.xlim - 1)
-        self.ax1.set_ylim(98000, 108000)  # Y AXIS, I THINK
+        # self.ax1.add_line(self.line2)
+        self.ax1.set_xlim(0, self.xlim - 1)  # REVERSE GRAPHING X AXIS HERE
+        self.ax1.set_ylim(YLOWER, YUPPER)
 
         FigureCanvas.__init__(self, self.fig)
         TimedAnimation.__init__(self, self.fig, interval=50, blit=True)
@@ -135,6 +170,30 @@ class CustomFigCanvas(FigureCanvas, TimedAnimation):
         self.ax1.set_ylim(bottom, top)
         self.draw()
 
+    def zoomOut(self, value):
+        bottom = self.ax1.get_ylim()[0]
+        top = self.ax1.get_ylim()[1]
+        bottom -= value
+        top += value
+        self.ax1.set_ylim(bottom, top)
+        self.draw()
+
+    def lessIn(self, value):
+        left = self.ax1.get_xlim()[0]
+        right = self.ax1.get_xlim()[1]
+        # left += value
+        right -= value
+        self.ax1.set_xlim(left, right)
+        self.draw()
+
+    def moreOut(self, value):
+        left = self.ax1.get_xlim()[0]
+        right = self.ax1.get_xlim()[1]
+        # left -= value
+        right += value
+        self.ax1.set_xlim(left, right)
+        self.draw()
+
     def _step(self, *args):
         # Extends the _step() method for the TimedAnimation class.
         try:
@@ -151,18 +210,23 @@ class CustomFigCanvas(FigureCanvas, TimedAnimation):
             self.y = np.roll(self.y, -1)
             self.y[-1] = self.addedData[0]
             del (self.addedData[0])
+            # np.ro
 
         self.line1.set_data(self.n[0: self.n.size - margin], self.y[0: self.n.size - margin])
-        self.line1_tail.set_data(np.append(self.n[-10:-1 - margin], self.n[-1 - margin]),
-                                 np.append(self.y[-10:-1 - margin], self.y[-1 - margin]))
+        self.line1_tail.set_data(np.append(self.n[-100:-1 - margin], self.n[-1 - margin]),
+                                 np.append(self.y[-100:-1 - margin], self.y[-1 - margin]))
         self.line1_head.set_data(self.n[-1 - margin], self.y[-1 - margin])
-        self._drawn_artists = [self.line1, self.line1_tail, self.line1_head]
+
+        # self.line2.set_data(self.n[1: self.n.size - margin], self.y[1: self.n.size - margin])
+        self._drawn_artists = [self.line1, self.line1_tail, self.line1_head]  # , self.line2]
 
 
-# You need to setup a signal slot mechanism, to
-# send data to your GUI in a thread-safe way.
-# Believe me, if you don't do this right, things
-# go very very wrong..
+        # self.line2_tail.set_data(np.append(self.n[-10:-1 - margin], self.n[-1 - margin]),
+        #                          np.append(self.y[-10:-1 - margin], self.y[-1 - margin]))
+        # self.line2_head.set_data(self.n[-1 - margin], self.y[-1 - margin])
+        # self._drawn_artists = [self.line2]
+
+
 class Communicate(QtCore.QObject):
     data_signal = QtCore.pyqtSignal(float)
 
@@ -172,24 +236,19 @@ def dataSendLoop(addData_callbackFunc):
     mySrc = Communicate()
     mySrc.data_signal.connect(addData_callbackFunc)
 
-    # Simulate some data
     n = np.linspace(0, 499, 500)
-    ##    cx, y = spew.bari.pop()
     i = 0
 
     while (True):
-        if (i > 1001):
+        if (i > 499):
             i = 0
-        time.sleep(0.01)
+        # pressure, _temperature = sensor.bari()
         pressure, _temperature = sensor.bari()
-        # y = sensor.read_altitude()
-        # y = sensor.read_temperature()
-
-        time.sleep(6)
+        ##############
+        time.sleep(SLEEP_FOR)  # (.052)  # Guestimated 1/10 second readinging with 5367 Chip lag
 
         mySrc.data_signal.emit(pressure)  # <- Here you emit a signal!
         i += 1
-        ###
 
 
 if __name__ == '__main__':
