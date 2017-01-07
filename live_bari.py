aa429c0a7e43ee746b16f4aa80381543d23627ab
Moe <moe@wadda.org>
2017-01-07 14:39:08 +1100

    Update live_bari.py

--- live_bari.py
+++ live_bari.py
@@ -1,9 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import bari
 from time import sleep
+import ms5637
 
-sensor = bari.Chip()
+__author__ = 'Moe'
+__copyright__ = 'Copyright 2017  Moe'
+__license__ = 'MIT'
+__version__ = '0.0.2'
+
+# Bari sensor of MS5637
+sensor = ms5637.Chip()
 
 plt.axis([0.0, 600.0, 100000, 102000])
 # plt.axis([0.0, 600.0, 15.0, 45.0])  # Temp
@@ -11,7 +17,7 @@ plt.ion()
 
 for i in range(600):
     #    sleep(2)
-    pressure, temperature = sensor.bari()
+    pressure, temperature = sensor.get_data()
 
     plt.scatter(i, pressure)
     #    plt.scatter(i, temperature)
